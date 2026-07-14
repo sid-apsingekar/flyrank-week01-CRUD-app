320ab69286458604cd75cf18e5bfd82e8fce4e4f
sid-apsingekar <siddarthapsingekar@gmail.com>
2026-07-14 23:18:58 +0530

    Stage 1:root and health endpoints

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ app = FastAPI()
 @app.get('/')
 def home():
     return {
-        'message':'hello from task api'
+        'name':'Task API',
+        'version':'1.0',
+        'endpoints':['/tasks']
+    }
+@app.get('/health')
+def health():
+    return {
+        'status':'ok'
     }
